70e2287add3b0bd6edd24561b0140846d98c1374
ootachi <furret@mimiga.net>
2010-09-18 13:40:40 -0700

    XML support for oEmbed

--- oembed.ml
+++ oembed.ml
@@ -1,7 +1,8 @@
 (* furretbot/oembed.ml *)
 
-module B = Json_type.Browse
-module Bu = Json_type.Build
+module EH = ExtHashtbl.Hashtbl
+module H = Hashtbl
+module Op = Option
 
 exception Malformed
 exception No_provider
@@ -39,15 +40,54 @@ type provider = {
     pr_endpoint: string;
 }
 
+type format = FO_json | FO_xml
+
 let backslash_star = lazy(Str.regexp_string "\\*")
 
 let embedly = [
     { pr_schemes = [ "*" ]; pr_endpoint = "http://api.embed.ly/v1/api/oembed" }
 ]
 
+let json_to_kvps json =
+    try
+        let json = Json_type.Browse.objekt (Json_io.json_of_string json) in
+        let json = Json_type.Browse.make_table json in
+        EH.map begin function
+            | Json_type.String s -> s
+            | Json_type.Int n -> string_of_int n
+            | _ -> raise Malformed
+        end json
+    with Json_type.Json_error _ -> raise Malformed
+
+let xml_to_kvps xml =
+    let xml = Xml.parse_string xml in
+    let kvps =
+        try
+            Xml.map begin fun child ->
+                (Xml.tag child, Xml.pcdata (List.hd (Xml.children child)))
+            end xml
+        with Xml.Not_element _ | Xml.Not_pcdata _ -> raise Malformed in
+    EH.of_enum (ExtList.List.enum kvps)
+
+let kvps_to_json kvps =
+    let kvps = List.map begin function
+        | (key, `String str) -> (key, Json_type.Build.string str)
+        | (key, `Int n) -> (key, Json_type.Build.int n)
+    end kvps in
+    Json_io.string_of_json (Json_type.Build.objekt kvps)
+
+let kvps_to_xml kvps =
+    let tags =
+        List.map begin function
+        | (key, `String str) -> Xml.Element(key, [], [ Xml.PCData str ])
+        | (key, `Int n) ->
+            Xml.Element(key, [], [ Xml.PCData(string_of_int n) ])
+    end kvps in
+    Xml.to_string(Xml.Element("oembed", [], tags))
+
 (**
  * Returns the provider for the given URL. Raises [No_provider] if none of the
- * URLs matched the provider.
+ * providers matched the URL.
  *)
 let provider_for_url ?providers:(providers=embedly) url =
     try
@@ -61,95 +101,112 @@ let provider_for_url ?providers:(providers=embedly) url =
     with Not_found -> raise No_provider
 
 (**
- * Performs an oEmbed request (using [Http_client.Convenience.http_get]) to
- * retrieve the embedded content for [url] and returns a [response] record.
+ * [request_url url] returns the URL to which a GET request should be made to
+ * retrieve the oEmbed response. Raises [No_provider] if none of the providers
+ * match the URL.
  *)
-let get ?providers:(providers=embedly) ?max_width:max_width
-        ?max_height:max_height url =
+let request_url ?providers:(providers=embedly) ?format:(format=FO_json)
+        ?max_width:max_width ?max_height:max_height url =
     let provider = provider_for_url ~providers:providers url in
 
     let params = DynArray.create() in
     DynArray.add params ("url", url);
-    DynArray.add params ("format", "json");
-    Option.may (fun n -> DynArray.add params ("maxwidth", (string_of_int n)))
+    begin
+        match format with
+        | FO_json -> DynArray.add params ("format", "json")
+        | FO_xml -> DynArray.add params ("format", "xml")
+    end;
+    Op.may (fun n -> DynArray.add params ("maxwidth", (string_of_int n)))
         max_width;
-    Option.may (fun n -> DynArray.add params ("maxheight", (string_of_int n)))
+    Op.may (fun n -> DynArray.add params ("maxheight", (string_of_int n)))
         max_height;
     let query_string = Netencoding.Url.mk_url_encoded_parameters
         (DynArray.to_list params) in
-    let body = Http_client.Convenience.http_get
-        (provider.pr_endpoint ^ "?" ^ query_string) in
 
-    let json = Json_io.json_of_string body in
+    provider.pr_endpoint ^ "?" ^ query_string
+
+(**
+ * [parse_response resp] parses the oEmbed response [resp] and returns a
+ * [response] record.
+ *)
+let parse_response ?format:(format=FO_json) resp =
+    let kvps =
+        match format with
+        | FO_json -> json_to_kvps resp
+        | FO_xml -> xml_to_kvps resp in
+    
     try
-        let root = B.make_table (B.objekt json) in
         let get_dimensions() =
             {
-                di_width = B.int (B.field root "width");
-                di_height = B.int (B.field root "height")
+                di_width = int_of_string (H.find kvps "width");
+                di_height = int_of_string (H.find kvps "height")
             }
         in
         let resource_type =
-            match B.string (B.field root "type") with
-            | "photo" ->
-                RT_photo(B.string (B.field root "url"), get_dimensions())
-            | "video" ->
-                RT_video(B.string (B.field root "html"), get_dimensions())
+            match Hashtbl.find kvps "type" with
+            | "photo" -> RT_photo(H.find kvps "url", get_dimensions())
+            | "video" -> RT_video(H.find kvps "html", get_dimensions())
             | "link" -> RT_link
-            | "rich" ->
-                RT_rich(B.string (B.field root "html"), get_dimensions())
+            | "rich" -> RT_rich(H.find kvps "html", get_dimensions())
             | _ -> raise Malformed
         in
         {
             re_type = resource_type;
-            re_title = B.optional B.string (B.fieldx root "title");
-            re_author_name = B.optional B.string (B.fieldx root "author_name");
-            re_author_url = B.optional B.string (B.fieldx root "author_url");
-            re_provider_name =
-                B.optional B.string (B.fieldx root "provider_name");
-            re_provider_url =
-                B.optional B.string (B.fieldx root "provider_url");
-            re_cache_age = B.optional B.int (B.fieldx root "cache_age");
-            re_thumbnail_url =
-                B.optional B.string (B.fieldx root "thumbnail_url");
+            re_title = EH.find_option kvps "title";
+            re_author_name = EH.find_option kvps "author_name";
+            re_author_url = EH.find_option kvps "author_url";
+            re_provider_name = EH.find_option kvps "provider_hame";
+            re_provider_url = EH.find_option kvps "provider_url";
+            re_cache_age =
+                Op.map int_of_string (EH.find_option kvps "cache_age");
+            re_thumbnail_url = EH.find_option kvps "thumbnail_url";
             re_thumbnail_width =
-                B.optional B.int (B.fieldx root "thumbnail_width");
+                Op.map int_of_string (EH.find_option kvps "thumbnail_width");
             re_thumbnail_height =
-                B.optional B.int (B.fieldx root "thumbnail_height");
-            re_description = B.string (B.field root "description")
+                Op.map int_of_string (EH.find_option kvps "thumbnail_height");
+            re_description = H.find kvps "description"
         }
-    with Json_type.Json_error _ | Not_found -> raise Malformed
+    with Not_found -> raise Malformed
 
-(** Returns the given oEmbed response as a JSON string. *)
-let put response =
+(**
+ * Performs an oEmbed request (using [Http_client.Convenience.http_get]) to
+ * retrieve the embedded content for [url] and returns a [response] record.
+ *)
+let get ?providers:(providers=embedly) ?format:(format=FO_json)
+        ?max_width:max_width ?max_height:max_height url =
+    let req_url = request_url ~providers:providers ~format:format
+        ?max_width:max_width ?max_height:max_height url in
+    let body = Http_client.Convenience.http_get req_url in
+    parse_response ~format:format body
+
+(** Returns the given oEmbed response as a JSON or XML string. *)
+let put ?format:(format=FO_json) response =
     let result = DynArray.create() in
     let add_dimensions dimensions =
-        DynArray.add result ("width", Bu.int dimensions.di_width);
-        DynArray.add result ("height", Bu.int dimensions.di_height)
+        DynArray.add result ("width", `Int dimensions.di_width);
+        DynArray.add result ("height", `Int dimensions.di_height)
     in
     let ty =
         match response.re_type with
         | RT_photo(url, dims) ->
             add_dimensions dims;
-            DynArray.add result ("url", (Bu.string url));
+            DynArray.add result ("url", `String url);
             "photo"
         | RT_video(html, dims) ->
             add_dimensions dims;
-            DynArray.add result ("html", (Bu.string html));
+            DynArray.add result ("html", `String html);
             "video"
         | RT_rich(html, dims) ->
             add_dimensions dims;
-            DynArray.add result ("html", (Bu.string html));
+            DynArray.add result ("html", `String html);
             "rich"
         | RT_link -> "link" in
-    DynArray.add result ("type", (Bu.string ty));
+    DynArray.add result ("type", `String ty);
    
     let add_string name =
-        Option.may (fun s -> DynArray.add result (name, (Bu.string s)))
+        Op.may (fun s -> DynArray.add result (name, `String s))
     in
-    let add_int name =
-        Option.may (fun n -> DynArray.add result (name, (Bu.int n)))
-    in
+    let add_int name = Op.may (fun n -> DynArray.add result (name, `Int n)) in
 
     add_string "title" response.re_title;
     add_string "author_name" response.re_author_name;
@@ -160,7 +217,9 @@ let put response =
     add_string "thumbnail_url" response.re_thumbnail_url;
     add_int "thumbnail_width" response.re_thumbnail_width;
     add_int "thumbnail_height" response.re_thumbnail_height;
-    DynArray.add result ("description", (Bu.string response.re_description));
+    DynArray.add result ("description", `String response.re_description);
 
-    Json_io.string_of_json (Bu.objekt (DynArray.to_list result))
+    match format with
+    | FO_json -> kvps_to_json (DynArray.to_list result)
+    | FO_xml -> kvps_to_xml (DynArray.to_list result)
 
